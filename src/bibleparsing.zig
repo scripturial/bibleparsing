efc0965909af0bafdb0122be3d8a35896b8887c0
Jay <git@scripturial.com>
2025-01-16 10:59:57 +0800

    Share pos and gender

--- src/bibleparsing.zig
+++ src/bibleparsing.zig
@@ -1,5 +1,7 @@
 pub const parse = @import("parse.zig").parse;
 pub const Parsing = @import("parsing.zig").Parsing;
+pub const Gender = @import("parsing.zig").Gender;
+pub const PartOfSpeech = @import("parsing.zig").PartOfSpeech;
 pub const Error = @import("parse.zig").Error;
 pub const byz = @import("byz.zig");
 pub const std = @import("std");
