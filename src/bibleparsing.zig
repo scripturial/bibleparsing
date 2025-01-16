aeb82dea9708e933134b3874a1eea202f81e77ae
Jay <git@scripturial.com>
2025-01-16 09:36:27 +0800

    Share Parsing

--- src/bibleparsing.zig
+++ src/bibleparsing.zig
@@ -1,3 +1,9 @@
 pub const parse = @import("parse.zig").parse;
+pub const Parsing = @import("parsing.zig").Parsing;
 pub const Error = @import("parse.zig").Error;
 pub const byz = @import("byz.zig");
+pub const std = @import("std");
+
+test {
+    std.testing.refAllDecls(@This());
+}
