cd1cbece2930ad02ab41f91c376d15c6a1be4599
Jay <git@scripturial.com>
2025-01-19 22:43:26 +0800

    Share parse_pos

--- src/bibleparsing.zig
+++ src/bibleparsing.zig
@@ -9,6 +9,7 @@ pub const byz = @import("byz.zig");
 pub const PartOfSpeech = @import("part_of_speech.zig").PartOfSpeech;
 pub const pos_to_string = @import("part_of_speech.zig").pos_to_string;
 pub const pos_to_camel_case = @import("part_of_speech.zig").pos_to_camel_case;
+pub const parse_pos = @import("part_of_speech.zig").parse_pos;
 
 test {
     std.testing.refAllDecls(@This());
